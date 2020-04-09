16923c29f87087909c12dd5ce1b37a1c24ecd62b
Gabriel Ecegi <gabriel.ecegi@blogic.cz>
2020-04-09 16:27:32 +0200

    Add optimist

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
 #!/usr/bin/env node
+import * as opt from "optimist";
 
-console.log(`whats up`);
+type ProgramProps = {
+  source: string | undefined;
+  help: never;
+};
+
+const optimist = opt
+  .usage("Usage: rest2ts -s path/to/swagger.json")
+  .alias("h", "help")
+  .alias("s", "source")
+  .describe("s", "Path to the swagger file");
+
+const { source, help } = optimist.argv as ProgramProps;
+
+if (help) {
+  optimist.showHelp();
+  process.exit(0);
+}
+
+if (source === undefined) {
+  console.error("Source -s not set");
+  process.exit(1);
+}
+
+console.log(`Getting openAPI from ${source}`);
