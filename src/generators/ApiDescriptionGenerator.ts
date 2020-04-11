88af5ac00ec98901d4129c1cd412850375163a28
Gabriel Ecegi <gabriel.ecegi@blogic.cz>
2020-04-11 15:14:51 +0200

    Polish route generation

--- src/generators/ApiDescriptionGenerator.ts
+++ src/generators/ApiDescriptionGenerator.ts
@@ -1,5 +1,4 @@
-import { Spec } from "swagger-schema-official";
-import { render } from "mustache";
+import { Spec, Path } from "swagger-schema-official";
 
 export type ApiDescription = { [pathName: string]: string };
 
@@ -9,12 +8,48 @@ const trimLast = (s: string, c: string) =>
 const trimStart = (s: string, c: string) =>
   s[0] === c ? s.substring(1, s.length) : s;
 
+const getPostFix = (path: Path) => {
+  if (path.get) {
+    return "GET";
+  }
+  if (path.post) {
+    return "POST";
+  }
+
+  if (path.put) {
+    return "PUT";
+  }
+
+  if (path.delete) {
+    return "DELETE";
+  }
+  if (path.patch) {
+    return "PATCH";
+  }
+  return "";
+};
+
+const formatPath = (str: string) =>
+  trimStart(trimLast(str.replace(/[\W_]+/g, "_"), "_"), "_");
+
+const getCommonPrefix = (str: string) => `${formatPath(str).split("_")[0]}_`;
+
 export const generateDescription = (spec: Spec) => {
   const api: ApiDescription = {};
+
+  const commonPrefix = Object.keys(spec.paths).reduce(
+    (acc, e) => (getCommonPrefix(e) === acc ? acc : ""),
+    getCommonPrefix(Object.keys(spec.paths)[0])
+  );
+
   Object.keys(spec.paths).forEach((e) => {
-    const k = spec.paths[e];
-    const prop = trimStart(trimLast(e.replace(/[\W_]+/g, "_"), "_"), "_");
-    api[prop] = e;
+    const pathObject = spec.paths[e];
+    const prop = formatPath(e).replace(commonPrefix, "");
+
+    const paramIndex = e.indexOf("{");
+    const path = paramIndex > 1 ? e.substring(0, paramIndex - 1) : e;
+
+    api[`${prop}_${getPostFix(pathObject)}`] = path;
   });
   return api;
 };
