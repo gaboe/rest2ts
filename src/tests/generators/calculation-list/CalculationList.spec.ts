b3dcc6e0203060df8a7e9b4600d2812172f13de1
Gabriel Ecegi <gabriel.ecegi@blogic.cz>
2020-04-12 17:02:21 +0200

    Add calculation list test

---
+++ src/tests/generators/calculation-list/CalculationList.spec.ts
@@ -0,0 +1,12 @@
+import test from "ava";
+import SwaggerParser from "@apidevtools/swagger-parser";
+import { generate } from "../../../lib/generators";
+
+test("parse calculation list", async (t) => {
+  var api = await SwaggerParser.parse(
+    "./src/tests/generators/calculation-list/swagger.json"
+  );
+  const content = generate(api, "");
+
+  t.snapshot(content);
+});
