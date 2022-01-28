54025feeaa789cd87db26002223cd5aba217e8be
Gabriel Ecegi <gabriel.ecegi@blogic.cz>
2022-01-28 16:03:35 +0100

    Add | null to nullable types

--- README.md
+++ README.md
@@ -7,3 +7,13 @@
 You do not need to install package locally/globally. It can be executed by npx
 
 > \$ npx rest2ts -s https://petstore.swagger.io/v2/swagger.json -t ./generated-output
+
+How to build and develop:
+
+> \$ npm install
+
+Optionally install TypeScript with
+
+> \$ npm install typescript -g
+
+> \$ npm run dev
--- src/lib/generators/ContractGenerator.ts
+++ src/lib/generators/ContractGenerator.ts
@@ -3,80 +3,79 @@ import { render } from "mustache";
 import { getTypeNameFromRef } from "./Common";
 import { Maybe, Just } from "purify-ts";
 
-const renderProperties = (
-  swagger: SwaggerSchema,
-  areNullableStringsEnabled: boolean
-) => (schema: Schema): string => {
-  if (schema.type === "object" && schema.properties) {
-    const properties = Object.keys(schema.properties)
-      .map((op) => {
-        const childProp = (schema.properties as any)[op] as Schema;
+const renderProperties =
+  (swagger: SwaggerSchema, areNullableStringsEnabled: boolean) =>
+  (schema: Schema): string => {
+    if (schema.type === "object" && schema.properties) {
+      const properties = Object.keys(schema.properties)
+        .map((op) => {
+          const childProp = (schema.properties as any)[op] as Schema;
 
-        const type = renderProperties(
-          swagger,
-          areNullableStringsEnabled
-        )(childProp);
+          const type = renderProperties(
+            swagger,
+            areNullableStringsEnabled
+          )(childProp);
 
-        const isNullable: boolean =
-          (childProp as any).nullable &&
-          //TODO rest of condition will be remove, when areNullableStringsEnabled will be deprecated
-          (type !== "string" ||
-            (type === "string" && areNullableStringsEnabled));
+          const isNullable: boolean =
+            (childProp as any).nullable &&
+            //TODO rest of condition will be remove, when areNullableStringsEnabled will be deprecated
+            (type !== "string" ||
+              (type === "string" && areNullableStringsEnabled));
 
-        const view = {
-          name: isNullable ? `${op}?` : op,
-          type: type,
-        };
-        return render("{{ name }}: {{ type }};", view);
-      })
-      .join("\n\t");
-    return properties;
-  } else if (schema.enum) {
-    return schema.enum.map((e) => `${e} = "${e}"`).join(",\n\t");
-  } else if (schema.allOf && schema.allOf[0]) {
-    const allOf = schema.allOf[0];
-    if (allOf.$ref) {
-      const typeName = getTypeNameFromRef(allOf.$ref);
-      const tt = swagger.components.schemas[typeName];
-      if (tt.type === "object") {
-        return typeName;
+          const view = {
+            name: isNullable ? `${op}?` : op,
+            type: isNullable ? `${type} | null` : type,
+          };
+          return render("{{ name }}: {{ type }};", view);
+        })
+        .join("\n\t");
+      return properties;
+    } else if (schema.enum) {
+      return schema.enum.map((e) => `${e} = "${e}"`).join(",\n\t");
+    } else if (schema.allOf && schema.allOf[0]) {
+      const allOf = schema.allOf[0];
+      if (allOf.$ref) {
+        const typeName = getTypeNameFromRef(allOf.$ref);
+        const tt = swagger.components.schemas[typeName];
+        if (tt.type === "object") {
+          return typeName;
+        }
+        return `typeof ${typeName}`;
       }
-      return `typeof ${typeName}`;
-    }
-    if (allOf.enum) {
-      return allOf.enum.map((e) => e).join(" | ");
-    }
-    if (allOf.type === "object") {
+      if (allOf.enum) {
+        return allOf.enum.map((e) => e).join(" | ");
+      }
+      if (allOf.type === "object") {
+        return "any";
+      }
+      return "any";
+    } else if (schema.type) {
+      switch (schema.type) {
+        case "integer":
+          return "number";
+        case "object":
+          return "unknown";
+        case "array":
+          const arrayTypeSchema = Maybe.fromNullable(schema.items)
+            .chain((e) => (e instanceof Array ? Just(e[0]) : Just(e)))
+            .chain((e) =>
+              Just(
+                e.$ref
+                  ? getTypeNameFromRef(e.$ref)
+                  : renderProperties(swagger, areNullableStringsEnabled)(e)
+              )
+            )
+            .orDefault("");
+          return `${arrayTypeSchema}[]`;
+        default:
+          return (schema.type || schema.allOf) as string;
+      }
+    } else if (schema.$ref) {
+      return schema.$ref.split("/").reverse()[0];
+    } else {
       return "any";
     }
-    return "any";
-  } else if (schema.type) {
-    switch (schema.type) {
-      case "integer":
-        return "number";
-      case "object":
-        return "unknown";
-      case "array":
-        const arrayTypeSchema = Maybe.fromNullable(schema.items)
-          .chain((e) => (e instanceof Array ? Just(e[0]) : Just(e)))
-          .chain((e) =>
-            Just(
-              e.$ref
-                ? getTypeNameFromRef(e.$ref)
-                : renderProperties(swagger, areNullableStringsEnabled)(e)
-            )
-          )
-          .orDefault("");
-        return `${arrayTypeSchema}[]`;
-      default:
-        return (schema.type || schema.allOf) as string;
-    }
-  } else if (schema.$ref) {
-    return schema.$ref.split("/").reverse()[0];
-  } else {
-    return "any";
-  }
-};
+  };
 
 export const generateContracts = (
   swaggerSchema: SwaggerSchema,
