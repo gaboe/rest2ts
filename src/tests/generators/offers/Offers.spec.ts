71a238d1dba4f5f8ba18d51cee75c184f7526083
Marek Rybarik <marek.rybarik@blogic.cz>
2023-06-15 15:36:19 +0200

    query params from schema.$ref fix

--- src/tests/generators/offers/Offers.spec.ts
+++ src/tests/generators/offers/Offers.spec.ts
@@ -2,11 +2,20 @@ import test from "ava";
 import SwaggerParser from "@apidevtools/swagger-parser";
 import { generate } from "../../../lib/generators";
 
-test("parse calculation list", async (t) => {
+test("parse calculation list", async t => {
   var api = await SwaggerParser.parse(
-    "./src/tests/generators/offers/swagger.json"
+    "./src/tests/generators/offers/swagger.json",
   );
   const content = await generate(api, "", undefined, true);
+
+  t.snapshot(content);
+});
+
+test("Angular parse calculation list", async t => {
+  var api = await SwaggerParser.parse(
+    "./src/tests/generators/offers/swagger.json",
+  );
+  const content = await generate(api, "", undefined, true, true);
 
   t.snapshot(content);
 });
