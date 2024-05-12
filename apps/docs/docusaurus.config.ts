39ccf439f8c8a2e8a0f021d954b153bcb9f425eb
Gabriel Ecegi <gabriel.ecegi@blogic.cz>
2024-05-12 06:39:56 +0200

    Configure gh pages

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -8,16 +8,16 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://gaboe.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/rest2ts/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "gaboe", // Usually your GitHub org/user name.
   projectName: "rest2ts", // Usually your repo name.
-
+  trailingSlash: false,
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
