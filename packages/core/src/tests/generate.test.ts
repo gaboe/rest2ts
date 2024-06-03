46f5a59b3d2eda8728adf4d4b6f35e0a510bc4ec
Gabriel Ecegi <gabriel.ecegi@blogic.cz>
2024-06-03 16:40:16 +0200

    Refactor tests

--- packages/core/src/tests/generate.test.ts
+++ packages/core/src/tests/generate.test.ts
@@ -2,6 +2,8 @@ import SwaggerParser from "@apidevtools/swagger-parser";
 import { describe, expect, test } from "vitest";
 import { generate } from "../lib/generators";
 
+const cases = ["petstore", "nswag"];
+
 function fixturePath(name: string) {
   return `./src/tests/fixtures/${name}.json`;
 }
@@ -10,33 +12,18 @@ function snapshotPath(name: string) {
   return `./__snapshots__/generate_${name}.ts`;
 }
 
-describe("PetStore API", () => {
-  test("Generate ", async () => {
-    const api = await SwaggerParser.parse(fixturePath("petstore"));
-    const content = await generate(api);
-    expect(content).toMatchFileSnapshot(snapshotPath("petstore"));
+for (const name of cases) {
+  describe(name, () => {
+    test("Generate", async () => {
+      const api = await SwaggerParser.parse(fixturePath(name));
+      const content = await generate(api);
+      expect(content).toMatchFileSnapshot(snapshotPath(name));
+    });
+
+    test("Generate angular", async () => {
+      const api = await SwaggerParser.parse(fixturePath(name));
+      const content = await generate(api, true, true);
+      expect(content).toMatchFileSnapshot(snapshotPath(`${name}_angular`));
+    });
   });
-
-  test("Generate angular", async () => {
-    const api = await SwaggerParser.parse(fixturePath("petstore"));
-    const content = await generate(api, true, true);
-    expect(content).toMatchFileSnapshot(snapshotPath("petstore_angular"));
-  });
-});
-
-describe("NSwag v14.0.3.0", () => {
-  test("Generate ", async () => {
-    const api = await SwaggerParser.parse(fixturePath("nswag"));
-    const content = await generate(api, false);
-
-    expect(content).toMatchFileSnapshot(snapshotPath("nswag"));
-  });
-
-  test("Generate angular", async () => {
-    const api = await SwaggerParser.parse(fixturePath("nswag"));
-
-    const content = await generate(api, true, true);
-
-    expect(content).toMatchFileSnapshot(snapshotPath("nswag_angular"));
-  });
-});
+}
