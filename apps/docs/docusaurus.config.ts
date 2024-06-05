9d49bd5af7a2241ff273b131ceb6c7972160242b
Gabriel Ecegi <gabriel.ecegi@blogic.cz>
2024-06-05 07:52:55 +0200

    Update social card

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -48,7 +48,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/docusaurus-social-card.png",
     navbar: {
       title: "Home",
       logo: {
