8bc076ffcbaf1faf9b2c83efbef16f79672a24b7
Marek Rybarik <marek.rybarik@blogic.cz>
2025-04-16 10:44:04 +0200

    fix: allOf schema with ref and missing type

--- apps/cli/src/tests/__snapshots__/generate_allof_anonymous_schema_angular.ts
+++ apps/cli/src/tests/__snapshots__/generate_allof_anonymous_schema_angular.ts
@@ -305,6 +305,11 @@ export type Dog = {
 	woof: boolean;
 };
 
+export type DogMissingType = Pet
+	& {
+	woof: boolean;
+};
+
 export type Cat = {
 	discriminator: string;
 	eat: boolean;
--- apps/cli/src/tests/__snapshots__/generate_allof_anonymous_schema_cookies.ts
+++ apps/cli/src/tests/__snapshots__/generate_allof_anonymous_schema_cookies.ts
@@ -392,6 +392,11 @@ export type Dog = {
 	woof: boolean;
 };
 
+export type DogMissingType = Pet
+	& {
+	woof: boolean;
+};
+
 export type Cat = {
 	discriminator: string;
 	eat: boolean;
